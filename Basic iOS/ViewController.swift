379c210a8b5c1c5b512c482d55f8c997cedd0a3a
renaldy <renalraion@gmail.com>
2021-01-13 14:53:38 +0700

    [add] adding learn-calculator for beginner

--- Basic iOS/ViewController.swift
+++ Basic iOS/ViewController.swift
@@ -9,11 +9,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var height: UITextField!
+    @IBOutlet weak var width: UITextField!
+    @IBOutlet weak var result: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func calculateArea(_ sender: UIButton) {
+        
+        if let h = Double(height.text ?? "0"), let w = Double(width.text ?? "0") {
+            result.text = String(h * w)
+        } else {
+            result.text = "Hasil tidak valid"
+        }
+        
+    }
+    @IBAction func resetView(_ sender: UIButton) {
+        
+        height.text = ""
+        width.text = ""
+        result.text = ""
+    }
 }
 
