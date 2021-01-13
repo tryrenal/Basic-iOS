3251d132a2158ccfcc551c083b758286fc82b41a
renaldy <renalraion@gmail.com>
2021-01-13 14:21:02 +0700

    Initial Commit

---
+++ Basic iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Basic iOS
+//
+//  Created by Renaldy on 13/01/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
