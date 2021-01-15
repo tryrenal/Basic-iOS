3ded1902119468cf865751d2ca1cde098424eed9
renaldy <renalraion@gmail.com>
2021-01-15 15:53:35 +0700

    [add] adding to learn rootview (change bg color and add button)

--- Basic iOS/ViewController.swift
+++ Basic iOS/ViewController.swift
@@ -12,6 +12,13 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .systemPink
+        
+        let button = UIButton(frame: CGRect(x: 184, y: 433, width: 70, height: 30))
+        button.setTitleColor(.white, for: .normal)
+        button.setTitle("Button", for: .normal)
+        self.view.addSubview(button)
     }
 
 
