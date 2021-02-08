9c530221481700756fad7be8f5253052899273f9
renaldy <renalraion@gmail.com>
2021-02-08 17:18:45 +0700

    [add] how to load image from url with extension

--- Basic iOS/ViewController.swift
+++ Basic iOS/ViewController.swift
@@ -10,13 +10,34 @@ import RealmSwift
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imagePreview: UIImageView!
+    
+    let imageUrl = "https://www.w3schools.com/w3css/img_lights.jpg"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    
         
+        guard let urlImage = URL(string: imageUrl) else {
+            return
+        }
+        
+        imagePreview.load(url: urlImage)
     }
 
 
+}
+
+extension UIImageView {
+    func load(url: URL){
+        DispatchQueue.global().async { [weak self] in
+            if let data = try? Data(contentsOf: url){
+                if let image = UIImage(data: data) {
+                    DispatchQueue.main.async {
+                        self?.image = image
+                    }
+                }
+            }
+        }
+    }
 }
 
